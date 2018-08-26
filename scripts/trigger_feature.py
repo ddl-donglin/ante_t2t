ae313a8d2c3300ba6fccd3f0ba2fb54848dfd187
daviddddl <daviddl997004866@gmail.com>
2018-08-26 19:05:22 +0800

    新增训练trigger代码和预测结果程序

--- scripts/trigger_feature.py
+++ scripts/trigger_feature.py
@@ -82,5 +82,5 @@ class TriggerFeature(text_problems.Text2ClassProblem):
 # t2t-trainer --t2t_usr_dir=. --data_dir=$DATA_DIR --problem=$PROBLEM_NAME --model=transformer --hparams_set=transformer_base --output_dir=$OUTPUT_DIR
 
 # DECODE_FROM = '../decode_in/sum_pooling_in.txt'
-# DECODE_TO = '../decode_out/sum_pooling_out.txt'
+# DECODE_TO = '../decode_out_truth/sum_pooling_out.txt'
 # t2t-decoder --t2t_usr_dir=./ --problem=$PROBLEM_NAME--data_dir=DATA_DIR --model=transformer --hparams_set=transformer_base --output_dir=$OUTPUT_DIR --decode_hparams=”beam_size=5,alpha=0.6” --decode_from_file=$DECODE_FROM --decode_to_file=$DECODE_TO
